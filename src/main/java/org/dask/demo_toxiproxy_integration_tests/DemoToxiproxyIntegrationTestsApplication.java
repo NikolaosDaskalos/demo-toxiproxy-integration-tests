d47029f85952a0df100c5c10776caf21c191f06a
Nikos Daskalos <daskgr@gmail.com>
2024-05-25 19:11:44 +0300

    Initial commit

---
+++ src/main/java/org/dask/demo_toxiproxy_integration_tests/DemoToxiproxyIntegrationTestsApplication.java
@@ -0,0 +1,13 @@
+package org.dask.demo_toxiproxy_integration_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToxiproxyIntegrationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToxiproxyIntegrationTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dask/demo_toxiproxy_integration_tests/DemoToxiproxyIntegrationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dask.demo_toxiproxy_integration_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoToxiproxyIntegrationTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
